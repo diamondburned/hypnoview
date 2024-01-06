93fb333bd59f24351b8248bd697580efb3ac65ae
diamondburned <diamond@arikawa-hi.me>
2024-01-06 02:03:28 -0800

    Add tests for EarliestTimestampForPeriod

--- lib/hypnohub/popular/popularity_times_test.go
+++ lib/hypnohub/popular/popularity_times_test.go
@@ -51,3 +51,49 @@ func TestTruncateWeek(t *testing.T) {
 		}
 	}
 }
+
+func TestEarliestTimestampForPeriod(t *testing.T) {
+	tests := []struct {
+		period TimePeriod
+		input  time.Time
+		output time.Time
+	}{
+		{
+			Daily,
+			time.Date(2024, time.January, 2, 23, 59, 59, 999, time.UTC),
+			time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Daily,
+			time.Date(2024, time.January, 2, 0, 0, 0, 0, time.UTC),
+			time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Weekly,
+			time.Date(2024, time.January, 6, 1, 2, 3, 456, time.UTC),
+			time.Date(2023, time.December, 25, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Weekly,
+			time.Date(2024, time.January, 7, 8, 9, 10, 11, time.UTC),
+			time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Monthly,
+			time.Date(2024, time.January, 14, 0, 0, 0, 0, time.UTC),
+			time.Date(2023, time.December, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			Monthly,
+			time.Date(2024, time.January, 15, 0, 0, 0, 0, time.UTC),
+			time.Date(2024, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+	}
+
+	for _, test := range tests {
+		output := EarliestTimestampForPeriod(test.input, test.period)
+		if output != test.output {
+			t.Errorf("%v %v: expected %v, got %v", test.period, test.input, test.output, output)
+		}
+	}
+}
